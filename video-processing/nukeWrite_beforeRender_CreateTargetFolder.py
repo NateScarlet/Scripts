674a3172cdcf8d64f1561cb7a8525676ba32b093
NateScarlet <NateScarlet@Gmail.com>
2016-09-21 18:46:14 +0800

    fix
    Former-commit-id: 9b9d2829b196299c07b7ec9c78bcbe955c314aff

--- video-processing/nukeWrite_beforeRender_CreateTargetFolder.py
+++ video-processing/nukeWrite_beforeRender_CreateTargetFolder.py
@@ -1,5 +1,9 @@
 file = nuke.tcl('eval list {'+nuke.thisNode()["file"].value()+'}');
 absolutePath = os.path.splitdrive(file)[0];
-project_directory = nuke.tcl('eval list {'+nuke.root()["project_directory"].value()+'}')+'/';
-pathHead = '' if absolutePath else project_directory;
-os.makedirs(pathHead+os.path.dirname(file));
+project_directory = nuke.tcl('eval list {'+nuke.root()["project_directory"].value()+'}');
+pathHead = '' if absolutePath else project_directory+'/';
+target = pathHead+os.path.dirname(file)
+if os.path.exists(target):
+    pass;
+else:
+    os.makedirs(target);
