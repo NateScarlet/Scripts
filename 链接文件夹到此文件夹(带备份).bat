dcfba791a2a04eb255baaade47230335128648ee
NateScarlet <NateScarlet@Gmail.com>
2016-09-28 17:36:38 +0800

    Fix
    Former-commit-id: 71d1bbdcc5d48371f04fc81f112a9148d9dca432

--- 链接文件夹到此文件夹(带备份).bat
+++ 链接文件夹到此文件夹(带备份).bat
@@ -1,9 +1,9 @@
 SET UserDir=被链接文件夹所在路径
-REM 文件夹名称可填多个文件夹名用逗号分隔
+REM 多个文件夹名用逗号分隔,格式为 "文件夹1","文件夹2",...
 FOR %%i in (文件夹名称) do (
-XCOPY /E /Y "%UserDir%\%%i" "%UserDir%\bak\%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%\%%i\"
-XCOPY /E /Y "%UserDir%\%%i" "%~dp0\%%i\"
-RMDIR /S /Q "%UserDir%\%%i"
-MKLINK /J "%UserDir%\%%i" "%~dp0%%i"
+XCOPY /E /Y "%UserDir%\%%~i" "%UserDir%\bak\%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%\%%~i\"
+XCOPY /E /Y "%UserDir%\%%~i" "%~dp0\%%~i\"
+RMDIR /S /Q "%UserDir%\%%~i"
+MKLINK /J "%UserDir%\%%~i" "%~dp0%%~i"
 )
 ::PAUSE
