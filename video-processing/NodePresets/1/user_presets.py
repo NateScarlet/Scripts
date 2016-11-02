e56e64402a551f2e56c5b3d4b7538516fb7c47d8
NateScarlet <NateScarlet@Gmail.com>
2016-11-02 19:14:10 +0800

    Update
    Former-commit-id: d3b7ae0af487b76b984a8e7dd4105bece9cc7bea

--- video-processing/NodePresets/1/user_presets.py
+++ video-processing/NodePresets/1/user_presets.py
@@ -1,4 +1,3 @@
 import nuke
 def nodePresetsStartup():
   nuke.setUserPreset("Expression", "1\/z", {'expr0': '(z==0)?1/z:100000', 'channel0': 'depth', 'selected': 'true', 'label': '1/z'})
-  nuke.setUserPreset("Expression", "1\z", {'expr0': '(z!=0)?1/z:100000', 'channel0': 'depth', 'selected': 'true', 'label': '1/z'})
