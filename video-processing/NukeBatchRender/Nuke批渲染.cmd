6c6e8e5cd67386e7f4c5d16ff2b54e8f864f121c
NateScarlet <NateScarlet@Gmail.com>
2016-11-23 11:28:00 +0800

    Nuke批渲染 - 修正不兼容带空格路径的问题
    Former-commit-id: 5b7a1226dc59e34137a05babd6bf9ec48900e332

--- video-processing/NukeBatchRender/Nuke批渲染.cmd
+++ video-processing/NukeBatchRender/Nuke批渲染.cmd
@@ -1,1 +1,2 @@
-START POWERSHELL "%~dp0NukeBatchRender.bat"
+START /WAIT POWERSHELL.exe -Command "chcp 65001"
+START POWERSHELL.exe -Command "& '%~dp0NukeBatchRender.bat'"
--- video-processing/NukeBatchRender/本地化全部素材.cmd
+++ video-processing/NukeBatchRender/本地化全部素材.cmd
@@ -1,1 +1,2 @@
-START POWERSHELL "%~dp0LocaliseFootage"
+START /WAIT POWERSHELL.exe -Command "chcp 65001"
+START POWERSHELL -Command "& '%~dp0LocaliseFootage'"
