3fef14082eb8fad24c6f33d70f5491c6c936de05
NateScarlet <NateScarlet@Gmail.com>
2016-12-04 23:21:18 +0800

    Update
    Former-commit-id: 6893d72917d651654d951f1b4f7954ae0ae34b5e

--- image-processing/压缩修图文件夹.bat
+++ image-processing/压缩修图文件夹.bat
@@ -1,3 +1,4 @@
+CHCP 936
 SET "MarkFile="%~1\修图 Nate.txt""
 ECHO 修图:Nate Scarlet 邮箱：NateScarlet@Gmail.com >> %MarkFile%
 ECHO 	非工作日乐意承接任何无偿漫画修图 通常于收到24小时内完成 >>%MarkFile%
@@ -5,3 +6,4 @@ ECHO 								%DATE% %TIME% >>%MarkFile%
 SET "zip="%ProgramFiles%\7-Zip\7z.exe""
 %zip% a "%~dp1[修图]%~n1.zip" "%~1\"
 DEL %MarkFile%
+PAUSE
