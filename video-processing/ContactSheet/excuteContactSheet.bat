04acf40733c467498c09b05a07d33d722e7e736e
NateScarlet <NateScarlet@Gmail.com>
2017-01-08 11:10:39 +0800

    Update
    Former-commit-id: e7188b19788841e6f6f63aec87d9610848239ff1

--- video-processing/ContactSheet/excuteContactSheet.bat
+++ video-processing/ContactSheet/excuteContactSheet.bat
@@ -1,7 +1,7 @@
 
 @ECHO off
 CHCP 65001
-TITLE 生成色板v1.2
+TITLE 生成色板v1.21
 SET "NUKE="C:\Program Files\Nuke10.0v4\Nuke10.0.exe""
 REM
 REM 在上方设置路径变量
@@ -9,12 +9,14 @@ REM
 CHDIR /D "%~dp0images"
 REM 删除缩略图缓存
 ATTRIB -S -H Thumbs.db
-DEL Thumbs.db 2>nul
-ECHO 删除代理文件:
-FOR /F %%i IN ('DIR /B "*_proxy.*"') DO (
-    ECHO %%~i
-    DEL "%%~i"
-    )
+DEL Thumbs.db 0>nul
+IF 1 == 0 (
+    ECHO 删除代理文件:
+    FOR /F %%i IN ('DIR /B "*_proxy.*"') DO (
+        ECHO %%~i
+        DEL "%%~i"
+        )
+)
 ECHO 删除临时文件:
 FOR /F %%i IN ('DIR /B "*.tmp"') DO (
      ECHO %%~i
--- video-processing/Py/Zdefocus_link.py
+++ video-processing/Py/Zdefocus_link.py
@@ -2,6 +2,7 @@ for i in nuke.allNodes():
     if 'ZDefocus' in i.Class() and 'Global' not in i['name'].value():
         i['size'].setExpression('ZDefocus_Global_1.size');
         i['max_size'].setExpression('ZDefocus_Global_1.max_size');
+        i['disable'].setExpression('([exists ZDefocus_Global_1]) ? !ZDefocus_Global_1.disable : 0');
     elif  'ZDefocus' in i.Class() and 'Global' in i['name'].value():
         i['max_size'].clearAnimated();
         i['max_size'].setValue(8);
