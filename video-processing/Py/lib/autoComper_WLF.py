e227e41fce312a091472228bd02b021b5a6bb724
NateScarlet <NateScarlet@Gmail.com>
2017-03-06 17:15:04 +0800

    update
    Former-commit-id: 079e9ef4e94fafb5870fdba746d628b5f0c31447

--- video-processing/Py/lib/autoComper_WLF.py
+++ video-processing/Py/lib/autoComper_WLF.py
@@ -45,28 +45,37 @@ def main():
 
 def placeNode(n):
     inputNum = n.inputs()
+    def _setNodeXY(n):
+        n.setXYpos(xpos, ypos)
+        nuke.autoplaceSnap(n)
+
+    
     if inputNum == 0 or n.Class() == 'Dot':
         return False
     input0 = n.input(0)
     xpos = input0.xpos()
-    ypos = input0.xpos()
-    print xpos
-    print ypos
-    ypos += 1000
+    ypos = input0.ypos()
+    print n.name() + str(xpos)+',' + str(ypos)
+    ypos += 200
     if inputNum == 1:
         n.setXYpos(xpos, ypos)
+        nuke.autoplaceSnap(n)
     elif inputNum == 2:
         n.setXYpos(xpos, ypos)
-        xpos += 200
-        if n.input(1).Class() == 'Dot':
-            n.input(1).setXYpos(xpos, ypos)
+
+        if n.input(1).Class() == 'Dot':\
+            dot = n.input(1)
+
         else:
             dot = nuke.nodes.Dot()
             dot.setInput(0, n.input(1))
             n.setInput(1, dot)
-            dot.setXYpos(xpos, ypos) 
-        ypos -= 500
-        n.input(1).input(0).setXYpos(xpos, ypos)
+            xpos += 100
+            _setNodeXY(dot)
+        xpos -= 50
+        ypos -= 200
+        input1 = n.input(1).input(0)
+        _setNodeXY(input1)
         
 main()
 
