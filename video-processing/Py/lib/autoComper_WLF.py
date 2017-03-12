dd5657608faeeee59a281a6da22d0d8cbe52238e
NateScarlet <NateScarlet@Gmail.com>
2017-03-12 17:48:17 +0800

    update [自动合成]实现简单merge
    Former-commit-id: 9d4afa6d0943b9483b7dde40b9776ad061bd84c0

--- video-processing/Py/lib/autoComper_WLF.py
+++ video-processing/Py/lib/autoComper_WLF.py
@@ -1,36 +1,72 @@
+#
+# -*- coding=UTF-8 -*-
+# WuLiFang Studio AutoComper
+# Version 0.1
+
+import nuke
+import os
 import re
 
-nodeFootageType = {}
+_dict_nodeTag = {}
+_nodes_mergeOver = []
+
+def main():
+
+    # Get all footage type
+    for i in nuke.allNodes():
+        if i.Class() == 'Read':
+            type = getFootageType(i)
+            _dict_nodeTag[i] = type
+    
+    if not _dict_nodeTag:
+        nuke.message('请先将素材拖入Nuke')
+        return False
+    
+    # MergeOver
+    mergeOver()
+    
+    # Create write node
+    _lastoutput.selectOnly() 
+    nuke.loadToolset("\\\\SERVER\scripts\NukePlugins\ToolSets\WLF\Write.nk")
+    
+    # Set framerange
+    _done = False
+    for i in _dict_nodeTag.keys():
+        if _dict_nodeTag[i] == 'CH_A':
+            nuke.Root()['first_frame'].setValue(i['first'].value())
+            nuke.Root()['last_frame'].setValue(i['last'].value())
+            nuke.Root()['lock_range'].setValue(True)
+            _done = True
+            break
+    if not _done:
+        nuke.message('没有找到CH_A：\n请手动设置工程帧范围')
 
 def getFootageType(n):
     '''
     Figure out node footage type
     '''
-
     _filename = nuke.filename(n)
     _s = os.path.basename(_filename)
     _pat = re.compile(r'_sc\d+_(.*?)\.')
     result = re.search(_pat, _s).group(1)
     return result
 
-def main():
-    # Get all footage type
-    for i in nuke.allNodes():
-        if i.Class() == 'Read':
-            type = getFootageType(i)
-            nodeFootageType[i] = type
-    # Simple merge
-    _nodes_simpleMerge = []
-    for i in nodeFootageType.keys():
-        if nodeFootageType[i] in ['BG_A', 'CH_A', 'CH_B', 'CH_C', 'CH_D']:
-            _nodes_simpleMerge.append(i)
+
+def mergeOver():
+
+    # Pick out node to mergeOver
+    for i in _dict_nodeTag.keys():
+        if _dict_nodeTag[i] in ['BG', 'BG_A', 'CH', 'CH_A', 'CH_B', 'CH_C', 'CH_D']:
+            _nodes_mergeOver.append(i)
+
     # Sort for processing order
-    _order_backfront = lambda n: nodeFootageType[n].replace('BG', '_1_').replace('CH', '_0_')
-    _nodes_simpleMerge.sort(key=_order_backfront, reverse=True)
+    _order_backfront = lambda n: _dict_nodeTag[n].replace('BG', '_1_').replace('CH', '_0_')
+    _nodes_mergeOver.sort(key=_order_backfront, reverse=True)
     
     # Create node
-    for i in _nodes_simpleMerge:
-        if _nodes_simpleMerge.index(i) == 0:
+    for i in _nodes_mergeOver:
+        global _lastoutput
+        if _nodes_mergeOver.index(i) == 0:
             _lastoutput = i
             continue
         _node_merge = nuke.nodes.Merge()
@@ -38,12 +74,18 @@ def main():
         _node_merge.setInput(1, i)
         _lastoutput = _node_merge
         
-    # Place node
+    # TODO:Place node
 
-        
-
+def mergeOCC():
+    # TODO
+    pass
+    
+def mergeShadow():
+    # TODO
+    pass
 
 def placeNode(n):
+    #TODO
     inputNum = n.inputs()
     def _setNodeXY(n):
         n.setXYpos(xpos, ypos)
@@ -76,8 +118,4 @@ def placeNode(n):
         ypos -= 200
         input1 = n.input(1).input(0)
         _setNodeXY(input1)
-        
-main()
 
-for i in nuke.allNodes():
-    placeNode(i)
