1e4b25578869e9693430fe5466eaaa0b4a582de7
NateScarlet <NateScarlet@Gmail.com>
2017-04-05 21:44:08 +0800

    update
    Former-commit-id: f470178cc3b4463dfc622fedf769741c329234cd

---
+++ bilibili-live/OBS.ahk
@@ -0,0 +1,2 @@
+RunWait, TASKKILL /f /im obs64.exe
+Run, obs64.exe, C:\Program Files (x86)\obs-studio\bin\64bit
--- bilibili-live/OBS.bat
+++
@@ -1,6 +0,0 @@
-CHCP 936
-
-TASKKILL /f /im obs64.exe
-CD /D "C:\Program Files (x86)\obs-studio\bin\64bit"
-
-START "" "obs64.exe"
