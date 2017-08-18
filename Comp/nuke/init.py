4dc12e06231eb72c3989c272b21fd68c8598ba82
NateScarlet <NateScarlet@Gmail.com>
2017-08-18 19:53:00 +0800

    turn cryptomatte to submodule
    Former-commit-id: 9f5d65e2926544b6380b4d5ea1ca31bb5ba0acfd

--- Comp/nuke/init.py
+++ Comp/nuke/init.py
@@ -2,7 +2,7 @@
 import nuke
 
 nuke.pluginAddPath('wlf')
-nuke.pluginAddPath('cryptomatte')
+nuke.pluginAddPath('cryptomatte/nuke')
 nuke.pluginAddPath('mamoworld')
 nuke.pluginAddPath('tabtabtab')
 nuke.pluginAddPath('batchrender')
