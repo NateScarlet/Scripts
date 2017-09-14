7ba2d297712a86fe40d1ab2988e2460702839f06
NateScarlet <NateScarlet@Gmail.com>
2017-09-14 15:39:03 +0800

    update
    Former-commit-id: 1ff6444d397b3fc60a44dcfa4fcdfa9fc0a0af96

--- Comp/nuke/validation.py
+++ Comp/nuke/validation.py
@@ -3,32 +3,15 @@
 
 import os
 import sys
-import pickle
-import datetime
+import datetime as time
 
-DATABASE = os.path.join(__file__, '../validation.db')
+EXPIRE_AT = time.date(2018, 1, 1)
 
+IS_EXPIRED = EXPIRE_AT < time.date.today()
+print('许可至: {}'.format(EXPIRE_AT))
+del EXPIRE_AT
 
-def __update_expire():
-    with open(DATABASE, 'wb') as f:
-        database = {'expire': datetime.date(2017, 1, 1)}
-        pickle.dump(database, f)
-
-
-def __is_expired():
-    try:
-        with open(DATABASE, 'r') as f:
-            database = pickle.load(f)
-            expire = database['expire']
-            print(u'吾立方插件: 许可至: {}'.format(expire).encode('gbk'))
-        return database['expire'] < datetime.date.today()
-    except:
-        return True
-
-
-# __update_expire()
-if __is_expired():
-    import nuke
-    nuke.message('吾立方插件: 许可已过期, 请联系作者获取更新许可\n要卸载请直接删除文件夹\n{}'.format(
+if IS_EXPIRED:
+    __import__('nuke').message('吾立方插件: 许可已过期, 请联系作者获取更新许可\n要卸载请直接删除文件夹\n{}'.format(
         os.path.abspath(os.path.join(__file__, '../../'))))
     sys.exit(0)
