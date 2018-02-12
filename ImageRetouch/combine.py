b3a6d2de916b5f473b8a140f446c3c7b1bb42ff9
NateScarlet <NateScarlet@Gmail.com>
2018-02-12 19:05:49 +0800

    Update handwriting suite

--- ImageRetouch/combine.py
+++ ImageRetouch/combine.py
@@ -1,87 +1,111 @@
 # -*- coding=UTF-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
 
-import os
+import argparse
 import locale
+import logging
+import os
+import sys
 
-VERSION = 0.1
-SYS_CODEC = locale.getdefaultlocale()[1]
+try:
+    _file = __file__
+    _args = sys.argv
+    import nuke
+except:
+    raise
 
-class Combine(object):
-    def __init__(self):
-        nuke.scriptClear()
-        os.chdir(r'E:\Porojects\handwriting\0620')
-        self._files = self.get_files()
-        self._tags = self.get_tags()
-        self.create_all()
 
-    def get_files(self):
-        _files = os.listdir(os.getcwd())
-        _files = map(lambda f : unicode(f, SYS_CODEC), _files)
-        return _files
+__version__ = '0.2.0'
 
-    def get_tags(self):
-        _tags = {}
-        for f in self._files:
-            _tag, _ext = os.path.splitext(f)
-            _tag = _tag[:-1]
-            if _ext.lower() not in ['.jpg', '.png']:
-                continue
-            _tags[_tag] = _tags.get(_tag, []) + [f]
-        return _tags
-            
-    def set_project(self):
-        nuke.Root()['first_frame'].setValue(1)
-        nuke.Root()['last_frame'].setValue(1)
-        nuke.Root()['project_directory'].setValue('[python {nuke.script_directory()}]')
-        
-    def create_all(self):
-        nuke.scriptClear()
-        for _tag, _files in self._tags.iteritems():
-            self.set_project()
-            self.create_combine(_tag)
-            #break
-            nuke.scriptSaveAndClear(u'{num}#{tag}.nk'.format(tag=_tag, num=len(self._tags[_tag])).encode('UTF-8'))
 
-    def create_combine(self, tag):
-        _outputs = []
-        for f in self._tags[tag]:
-            _read = nuke.nodes.Read(file=f.encode('UTF-8'))
-            _mirror = nuke.nodes.Mirror2(inputs=[_read])
-            _crop = nuke.nodes.Crop(inputs=[_mirror])
-            _trans = nuke.nodes.Transform(inputs=[_crop])
-            _outputs.append(_trans)
-        _merge = nuke.nodes.Merge2(inputs=_outputs)
-        _crop2 = nuke.nodes.Crop(
-            inputs=[_merge],
-            box=
-                '{{\[python nuke.thisNode().input(0).bbox().x()]}} '\
-                '{{\[python nuke.thisNode().input(0).bbox().y()]}} '\
-                '{{\[python nuke.thisNode().input(0).bbox().w()\\ +\\ nuke.thisNode().input(0).bbox().x()]}} '\
-                '{{\[python nuke.thisNode().input(0).bbox().h()\\ +\\ nuke.thisNode().input(0).bbox().y()]}}',
-                reformat=True
-        )
-        _expr = nuke.nodes.Expression(inputs=[_crop2], channel0='rgba', expr0='1')
-        _merge2 = nuke.nodes.Merge2(inputs=[_crop2, _expr], operation='under')
-        _write = nuke.nodes.Write(
-            inputs=[_merge2], 
-            file=''.join([tag, '.jpg']).encode('UTF-8'), 
-            file_type='jpeg',
-            _jpeg_quality=1.0,
-        )
-        
-        self.autoplace_all()
+def script_setup():
+    nuke.scriptClear()
+    nuke.Root()['first_frame'].setValue(1)
+    nuke.Root()['last_frame'].setValue(1)
+    nuke.Root()['project_directory'].setValue(
+        '[python {nuke.script_directory()}]')
 
-    @staticmethod
-    def autoplace_all():
-        for n in nuke.allNodes():
-            nuke.autoplace(n)
+
+def autoplace_all():
+    for n in nuke.allNodes():
+        nuke.autoplace(n)
+
+
+def get_files(dir_):
+    ret = os.listdir(dir_)
+    ret = [unicode(i, sys.getfilesystemencoding()) for i in ret]
+    ret = [i for i in ret if i.lower().endswith(('.jpg', '.png'))]
+    return ret
+
+
+def get_tags(files):
+    ret = {}
+    for f in files:
+        name, _ = os.path.splitext(f)
+        tag = name[:-1]
+        ret.setdefault(tag, [])
+        ret[tag].append(f)
+    return ret
+
+
+def create_combine(files, tag):
+    _outputs = []
+    for f in files:
+        _read = nuke.nodes.Read(file=f.encode('UTF-8'))
+        _mirror = nuke.nodes.Mirror2(inputs=[_read])
+        _crop = nuke.nodes.Crop(inputs=[_mirror],
+                                )
+        _trans = nuke.nodes.Transform(inputs=[_crop])
+        _outputs.append(_trans)
+    _merge = nuke.nodes.Merge2(inputs=_outputs)
+    _crop2 = nuke.nodes.Crop(
+        inputs=[_merge],
+        box='{{\[python nuke.thisNode().input(0).bbox().x()]}} '
+        '{{\[python nuke.thisNode().input(0).bbox().y()]}} '
+        '{{\[python nuke.thisNode().input(0).bbox().w()\\ +\\ nuke.thisNode().input(0).bbox().x()]}} '
+            '{{\[python nuke.thisNode().input(0).bbox().h()\\ +\\ nuke.thisNode().input(0).bbox().y()]}}',
+            reformat=True
+    )
+    _expr = nuke.nodes.Expression(
+        inputs=[_crop2], channel0='rgba', expr0='1')
+    _merge2 = nuke.nodes.Merge2(inputs=[_crop2, _expr], operation='under')
+    filename = '{}.jpg'.format(tag)
+    e_filename = filename.encode('utf-8')
+    _write = nuke.nodes.Write(
+        inputs=[_merge2],
+        file=e_filename,
+        proxy=e_filename,
+        file_type='jpeg',
+        _jpeg_quality=1.0,
+    )
+
 
 def main():
-    Combine()
-    
+    parser = argparse.ArgumentParser(_file)
+    parser.add_argument('dir', metavar='working_dir')
+    args = parser.parse_args(_args[1:])
+
+    logging.basicConfig(level=logging.DEBUG)
+    files = get_files(args.dir)
+    logging.debug(files)
+    tags = get_tags(files)
+    logging.debug(tags)
+    nuke.scriptClear()
+
+    for tag, files in tags.items():
+        script_setup()
+        create_combine(files, tag)
+        autoplace_all()
+        filename = u'{}/{num}#{tag}.nk'.format(args.dir,
+                                               tag=tag, num=len(files))
+        nuke.scriptSave(filename.encode('UTF-8'))
+
+
 if __name__ == '__main__':
     try:
         main()
+    except SystemExit:
+        pass
     except:
         import traceback
         traceback.print_exc()
