33c3e7401021609b4ac6692dfceb5bd23bb66d2f
NateScarlet <NateScarlet@Gmail.com>
2019-01-31 10:31:41 +0800

    Update

--- setup_gitproxy.cmd
+++ setup_gitproxy.cmd
@@ -1,5 +1,6 @@
 setx http_proxy 127.0.0.1:1080
 setx https_proxy 127.0.0.1:1080
+setx no_proxy 127.0.0.1,192.168.*.*,localhost
 git config --global http.https://golang.org.proxy 127.0.0.1:1080
 git config --global http.https://go.googlesource.com.proxy 127.0.0.1:1080
 git config --global http.https://go.googlesource.com.sslVerify false
