9da902f18a82c5762360985c616b20ac8e5db94d
NateScarlet <NateScarlet@Gmail.com>
2019-01-31 23:42:17 +0800

    Update

--- Greasemonkey/刺猬猫公共章节自动下载.js
+++ Greasemonkey/刺猬猫公共章节自动下载.js
@@ -1,3 +1,11 @@
+// ==UserScript==
+// @name     刺猬猫公共章节自动下载
+// @version  1
+// @grant    none
+// @include	 https://www.ciweimao.com/chapter/*
+// @run-at   document-idle
+// ==/UserScript==
+
 (function() {
   const element = document.querySelector("#J_BookRead");
   const chapter = document.querySelector("#J_BookCnt h3.chapter").firstChild
@@ -12,9 +20,9 @@
   });
   const anchor = document.createElement("a");
   anchor.href = URL.createObjectURL(file);
-  anchor.download = `${
-    location.pathname.split("/").slice(-1)[0]
-  }_${chapter}.txt`;
+  anchor.download = `${location.pathname.split("/").slice(-1)[0]} ${
+    document.title
+  }.txt`;
   anchor.style["display"] = "none";
   document.body.append(anchor);
   anchor.click();
