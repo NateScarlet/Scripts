fa846c026f81bd7873f9cdea946cb5803f82ce33
NateScarlet <NateScarlet@Gmail.com>
2019-02-20 23:33:44 +0800

    Add `sudo.cmd`

---
+++ sudo.cmd
@@ -0,0 +1,1 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Start-Process -Wait -FilePath powershell.exe -ArgumentList @('-NoProfile', '-InputFormat', 'None', '-ExecutionPolicy', 'Bypass', '-Command', 'cd', '%cd%', ';', '%*') -verb RunAs" 
