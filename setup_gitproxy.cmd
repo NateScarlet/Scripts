738bf9b545d4309752372d14cb7bb3df4125364b
NateScarlet <NateScarlet@Gmail.com>
2019-02-21 11:45:20 +0800

    Update `setup_gitproxy`

--- setup_gitproxy.cmd
+++ setup_gitproxy.cmd
@@ -1,6 +1,7 @@
-setx http_proxy 127.0.0.1:1080
-setx https_proxy 127.0.0.1:1080
-setx no_proxy 127.0.0.1,192.168.*.*,localhost
-git config --global http.https://golang.org.proxy 127.0.0.1:1080
-git config --global http.https://go.googlesource.com.proxy 127.0.0.1:1080
-git config --global http.https://go.googlesource.com.sslVerify false
+SET "PROXY=127.0.0.1:1080"
+REM setx http_proxy 127.0.0.1:1080
+REM setx https_proxy 127.0.0.1:1080
+REM setx no_proxy 127.0.0.1,192.168.*.*,localhost
+ping -n 1 golang.org || git config --global http.https://golang.org.proxy "%PROXY%"
+ping -n 1 github.com || git config --global http.https://github.com.proxy "%PROXY%"
+ping -n 1 go.googlesource.com || git config --global http.https://go.googlesource.com.proxy "%PROXY%" && git config --global http.https://go.googlesource.com.sslVerify false
