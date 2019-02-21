96e5db680b21db4622cfd9fd172e7945732556df
NateScarlet <NateScarlet@Gmail.com>
2019-02-21 22:16:58 +0800

    Improve `sudo.cmd`

--- sudo.cmd
+++ sudo.cmd
@@ -2,5 +2,5 @@ CALL :SUDO %*
 GOTO :EOF
 
 :SUDO
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Start-Process -Wait -FilePath powershell.exe -ArgumentList @('-NoProfile', '-InputFormat', 'None', '-ExecutionPolicy', 'Bypass', '-Command', 'cd', '%cd%', ';', '%*') -verb RunAs" 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Start-Process -Wait -FilePath powershell.exe -ArgumentList @('-NoProfile', '-InputFormat', 'None', '-ExecutionPolicy', 'Bypass', '-Command', 'cd', '%cd%', ';', """%*""") -verb RunAs" 
 GOTO :EOF
