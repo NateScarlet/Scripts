eb0960f31db00880553b2fe13c6e03468187aa51
NateScarlet <NateScarlet@Gmail.com>
2019-03-12 13:52:19 +0800

    Update github_anchor_enhance.js

--- Greasemonkey/github_anchor_enhance.js
+++ Greasemonkey/github_anchor_enhance.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Github anchor enhance
-// @version  1
+// @version  2
 // @grant    GM.xmlHttpRequest
 // @run-at   document-idle
 // @include	 *
@@ -13,11 +13,9 @@
       if (e.target && e.target.nodeName == 'A') {
         /** @type {HTMLAnchorElement} */
         const el = e.target;
-        if (el.classList.contains('added-starts-badge')) {
-          return;
-        }
+
         appendStarsBadge(el);
-        el.classList.add('added-starts-badge');
+        appendFollowersBadge(el);
       }
     },
     {}
@@ -29,6 +27,10 @@
  * @param {HTMLAnchorElement} el
  */
 async function appendStarsBadge(el) {
+  const className = 'added-stars-badge';
+  if (el.classList.contains(className)) {
+    return;
+  }
   const match =
     el.href && el.href.match(/https:\/\/github.com\/([^\/]+)\/([^\/]+)$/);
 
@@ -40,6 +42,33 @@ async function appendStarsBadge(el) {
       onload: resp => {
         if (resp.status === 200) {
           el.innerHTML += resp.response;
+          el.classList.add(className);
+        }
+      },
+      onerror: console.error
+    });
+  }
+}
+/**
+ *
+ * @param {HTMLAnchorElement} el
+ */
+async function appendFollowersBadge(el) {
+  const className = 'added-followers-badge';
+  if (el.classList.contains(className)) {
+    return;
+  }
+  const match = el.href && el.href.match(/https:\/\/github.com\/([^\/]+)$/);
+
+  if (match) {
+    const [_, user] = match;
+    GM.xmlHttpRequest({
+      method: 'GET',
+      url: `https://img.shields.io/github/followers/${user}.svg?style=social`,
+      onload: resp => {
+        if (resp.status === 200) {
+          el.innerHTML += resp.response;
+          el.classList.add(className);
         }
       },
       onerror: console.error
