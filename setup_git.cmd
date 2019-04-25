14b30a4cf50c5013d9ecff5dd9d15958554a82d1
NateScarlet <NateScarlet@Gmail.com>
2019-04-25 20:15:46 +0800

    update

--- setup_git.cmd
+++ setup_git.cmd
@@ -3,3 +3,5 @@ git config --global core.editor "code.cmd --wait --reuse"
 git config --global core.autocrlf input
 git config --global push.followTags true
 git config --global alias.amend "commit --amend --no-edit"
+REM https://hub.github.com/hub.1.html
+git config --global hub.protocol https
