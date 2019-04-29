1751667a8e8f95de71d21c531c3488ba060d9de9
NateScarlet <NateScarlet@Gmail.com>
2019-04-29 11:59:44 +0800

    update

--- setup_git.cmd
+++ setup_git.cmd
@@ -5,3 +5,6 @@ git config --global push.followTags true
 git config --global alias.amend "commit --amend --no-edit"
 REM https://hub.github.com/hub.1.html
 git config --global hub.protocol https
+REM https://stackoverflow.com/questions/14937405/npm-git-protocol-dependencies
+git config --global url."https://github.com/".insteadOf git@github.com:
+git config --global url."https://".insteadOf git://
