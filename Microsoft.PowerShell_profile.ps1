5f44f3bb4356418479d6fb7e893d3e275a5a4a2d
NateScarlet <NateScarlet@Gmail.com>
2020-09-30 19:01:19 +0800

    update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,14 @@
+# https://github.com/PowerShell/PSReadLine/issues/779
+Remove-Module -ErrorAction SilentlyContinue PSReadLine
+
+# Refresh env
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+
+# Config
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+function prompt {
+    $p = (Get-Location)
+    $p = ($p -replace (":\\Users\\" + [Environment]::UserName), ":\~")
+    "PS $p> "
+}
