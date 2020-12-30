429878320a9fde8b67d1a51f224725281368b16a
NateScarlet <NateScarlet@Gmail.com>
2020-12-30 14:00:22 +0800

    add Build-PS1ScriptAsCmd.ps1

---
+++ Build-PS1ScriptAsCmd.ps1
@@ -0,0 +1,21 @@
+Param (
+    [Parameter(
+        Mandatory,
+        Position = 0,
+        HelpMessage="Script file path"
+    )]
+    [string]
+    $Path
+)
+
+# https://stackoverflow.com/a/65314841
+$launch_code = @'
+@echo off
+PowerShell -Command "Get-Content '%~dpnx0' -Encoding UTF8 | Select-Object -Skip 5 | Out-String | Invoke-Expression"
+IF ERRORLEVEL 1 PAUSE
+GOTO :EOF
+
+
+'@ -replace "`n","`r`n"
+
+Get-Content $Path | Join-String -Separator "`r`n" -OutputPrefix $launch_code | Set-Content -Encoding UTF8 "$Path.cmd"
