f789c24679cd4daaeb83287dc3122866668411d8
NateScarlet <NateScarlet@Gmail.com>
2021-03-09 16:52:59 +0800

    update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,8 +7,12 @@ if ((Get-Module -Name PSReadLine).Version.Major -eq 2) {
 # Refresh env
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
-# Config
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+# https://bugs.python.org/issue42627
+$proxy = (get-itemproperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').ProxyServer
+if ($proxy) {
+    $env:HTTP_PROXY = "http://$proxy"
+    $env:HTTPS_PROXY = "http://$proxy"
+}
 
 function prompt {
     $p = (Get-Location)
