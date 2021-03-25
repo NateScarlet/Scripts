182aaae08b916a6059e6888b56f35ca5c384dc5b
NateScarlet <NateScarlet@Gmail.com>
2021-03-25 20:49:43 +0800

    add: unset_proxy.cmd

---
+++ unset_proxy.cmd
@@ -0,0 +1,2 @@
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f 
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "" /f 
