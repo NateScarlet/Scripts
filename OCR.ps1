81a6e5fc5793c291d45cb453e6fa9c59c5b26c79
NateScarlet <NateScarlet@Gmail.com>
2021-05-26 17:20:27 +0800

    feat(OCR): improve clipboard data

--- OCR.ps1
+++ OCR.ps1
@@ -11,13 +11,38 @@ Param (
     $ToClipboard
 )
 
-$result = & "${env:ProgramFiles}\Tesseract-OCR\tesseract.exe" $Path -
+[String]$result = & "${env:ProgramFiles}\Tesseract-OCR\tesseract.exe" $Path -
 if ($LASTEXITCODE) {
     exit $LASTEXITCODE
 }
 
+$result = $result.Replace("`f", "`n`n").Trim()
+
 if ($ToClipboard) {
-    Set-Clipboard $result
-} else {
+    Add-Type -AssemblyName PresentationCore
+    $img = [System.Windows.Media.Imaging.BitmapImage]::new($Path)
+    $do = [System.Windows.DataObject]::new()
+    $do.SetImage($img)
+    $do.SetText($result.Trim())
+    $do.SetData("HTML Format", @"
+<img src=`"file:///$Path`">
+<dl>
+"@ + @(If ($result) {
+                @"
+    <dt>文本识别</dt>
+    <dd>$result</dd>
+"@             
+            }
+            Else { "" }
+        ) + @"
+    <dt>文件名</dt>
+    <dd>$(Split-Path -Leaf $Path)</dd>
+    <dt>分辨率</dt>
+    <dd>$([int]$img.Width)x$([int]$img.Height)</dd>
+</dl>
+"@)
+    [System.Windows.Clipboard]::SetDataObject($do, $true)
+}
+else {
     $result
 }
