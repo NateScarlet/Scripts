111d2f716ac8ba83afab60521edc3e42794c4a0f
NateScarlet <NateScarlet@Gmail.com>
2021-06-28 14:23:45 +0800

    update git config
    always sign tags

--- setup_git.cmd
+++ setup_git.cmd
@@ -3,6 +3,7 @@ git config --global user.name "NateScarlet"
 git config --global core.editor "code.cmd --wait"
 git config --global core.autocrlf input
 git config --global commit.gpgsign true
+git config --global tag.gpgsign true
 git config --global user.signingkey 5C242793B070309C
 git config --global gpg.program "C:\Program Files (x86)\GnuPG\bin\gpg.exe"
 git config --global push.followTags true
