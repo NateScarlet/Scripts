a8bac455c6b2501c94772ae23bd9ca9fac00edf1
NateScarlet <NateScarlet@Gmail.com>
2025-02-14 17:54:57 +0800

    add daily.ps1

---
+++ daily.ps1
@@ -0,0 +1,5 @@
+# update stretchly stamp without network
+if (Test-Path -Path "$env:APPDATA\Stretchly") {
+    Get-date -AsUTC  -Format o  |  Out-File -NoNewline "$env:APPDATA\Stretchly\stamp"
+    "did update stretchly stamp"
+}
