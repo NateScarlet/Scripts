c39d928e4c316518f38b0021592ebcc4e7a8efe9
NateScarlet <NateScarlet@Gmail.com>
2025-05-07 10:16:17 +0800

    update daily.ps1

--- daily.ps1
+++ daily.ps1
@@ -3,3 +3,5 @@ if (Test-Path -Path "$env:APPDATA\Stretchly") {
     Get-date -AsUTC  -Format o  |  Out-File -NoNewline "$env:APPDATA\Stretchly\stamp"
     "did update stretchly stamp"
 }
+
+& "$PSScriptRoot\maintenance\Sync-Rime.ps1"
