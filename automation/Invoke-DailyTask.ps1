961331c7f0638934788e8354ba02eb71e42c68ca
NateScarlet <NateScarlet@Gmail.com>
2025-11-20 09:43:31 +0800

    update Invoke-DailyTask.ps1

--- automation/Invoke-DailyTask.ps1
+++ automation/Invoke-DailyTask.ps1
@@ -1,9 +1,10 @@
+$ErrorActionPreference="Stop"
 # update stretchly stamp without network
 if (Test-Path -Path "$env:APPDATA\Stretchly") {
     Get-date -AsUTC  -Format o  |  Out-File -NoNewline "$env:APPDATA\Stretchly\stamp"
     "did update stretchly stamp"
 }
 
-& "$PSScriptRoot\automation\Sync-Rime.ps1"
-& "$PSScriptRoot\automation\Invoke-AutoGitCommit.ps1"
-& "$PSScriptRoot\automation\Delete-Old-Download.ps1"
+& "$PSScriptRoot\Sync-Rime.ps1"
+& "$PSScriptRoot\Invoke-AutoGitCommit.ps1"
+& "$PSScriptRoot\Delete-Old-Download.ps1"
