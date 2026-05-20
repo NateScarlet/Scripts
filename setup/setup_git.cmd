2aacb23e5649c5600e1ac6678bb4fee0f026d8fa
NateScarlet <NateScarlet@Gmail.com>
2026-05-20 12:16:23 +0800

    update setup_git.cmd

--- setup/setup_git.cmd
+++ setup/setup_git.cmd
@@ -6,7 +6,7 @@ git config --global core.autocrlf input
 git config --global commit.gpgsign true
 git config --global tag.gpgsign true
 git config --global user.signingkey 5C242793B070309C
-git config --global gpg.program "C:\Program Files (x86)\GnuPG\bin\gpg.exe"
+git config --global gpg.program "C:\Program Files\GnuPG\bin\gpg.exe"
 git config --global push.followTags true
 git config --global alias.amend "commit --amend --no-edit"
 REM https://hub.github.com/hub.1.html
@@ -14,3 +14,4 @@ git config --global hub.protocol https
 REM https://stackoverflow.com/questions/14937405/npm-git-protocol-dependencies
 git config --global url."https://github.com/".insteadOf git@github.com:
 git config --global url."https://".insteadOf git://
+git config --global init.defaultBranch main
